2dc0efd7f3029b1af94a25935234f7bf7ceee73b
unknown <gregmasterbr@gmail.com>
2022-11-24 21:08:37 -0300

    Configurando o uso do BD MYSQL

--- calculadorafatec/settings.py
+++ calculadorafatec/settings.py
@@ -90,13 +90,22 @@ WSGI_APPLICATION = 'calculadorafatec.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# settings.py
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'contrib/mysql.cnf'),
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
